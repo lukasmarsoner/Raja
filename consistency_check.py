5df8b0b7970706646452098e45d8ad0897877c83
Lukas Marsoner <lukas.marsoner@gmail.com>
2019-04-29 22:21:27 +0200

    First Ending - beginning work on ending 2

--- consistency_check.py
+++ consistency_check.py
@@ -19,7 +19,7 @@ totalNumerOfPaths = 0
 atEnd = False
 
 #We will run through all possible paths - endings will
-#be marked by fowarting to 999
+#be marked by fowarting to 888
 i = 0
 while not(atEnd):
     #See if we have been here before
@@ -34,6 +34,12 @@ while not(atEnd):
         openOptions.pop(newOpt)
     else:
         if len(screens[i]['forwards']) > 1 and i not in branchesSeen:
+            #Check that all lists have the required length
+            if len(screens[i]['forwards']) != len(screens[i]['forwards']) or
+               len(screens[i]['forwards']) != len(screens[i]['conditions']) or
+               len(screens[i]['forwards']) != len(screens[i]['erlebnisse']):
+                   raise ValueError('Arrays at screen {} have inconsistend length'.format(indexes[i]))
+            
             #Make sure we dont get back to branching-points
             #Already seen and add old paths again
             branchesSeen.append(i)
@@ -42,17 +48,17 @@ while not(atEnd):
                 openScreens.append(i)
         #Find the array-index the forward refers to
         try:
-            if screens[i]['forwards'][0] != 999:
+            if screens[i]['forwards'][0] != 888:
                 forward = indexes.index(screens[i]['forwards'][0])
             else:
-                forward = 999
+                forward = 888
         except:
             print('Incorrect Forward')
         #Check loops - we should not see the same screen more than ten times
         if max(screensSeenCounter) > 10:
             raise ValueError('Circular Forward found at screen {}'.format(indexes[i]))
         #Check if we have reached an endpoint
-        if forward == 999:
+        if forward == 888:
             #End-Screens can be reached as many times as we want
             #We set back the counter to falsly identify loops involving endpoints
             screensSeenCounter[i] = 0
