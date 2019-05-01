6bc041af8e1de6b57a916522f5ffe1305ee4d0c1
Lukas Marsoner <lukas.marsoner@gmail.com>
2019-05-01 20:25:26 +0200

    First test of CD

---
+++ firebase_upload.py
@@ -0,0 +1,35 @@
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+with open('geschichte.json', 'r', encoding='utf-8') as jsonFile:
+    jsonStr = jsonFile.read()
+    storyIn = json.loads(jsonStr)
+
+metadata = ['name','version','image']
+#Add all data but the screens to the output
+storyOut = {}
+for key in metadata:
+    storyOut[key] = storyIn[key]
+
+#Transform lists into dictionaries for firebase-upload
+for i in range(len(storyIn['screens'])):
+    _screen = {}
+    for key in storyIn['screens'][i].keys():
+        if key not in ['number', 'text']:
+            _map = {}
+            for j,value in enumerate(storyIn['screens'][i][key]):
+                #Use only strings for compatability to json/firebase
+                _map[str(j)] = str(value)
+            _screen[key] = _map
+    storyOut[str(storyIn['screens'][i]['number'])] = _screen
+
+#Add number of screens
+storyOut['nscreens'] = len(storyIn['screens'])
+
+#Here we actually upload the data to firebase
+cred = credentials.Certificate(hundetage.key)
+db = firestore.Client(credentials=cred)
+
+colRef = db.collection('abenteuer').document(story['name'])
+
