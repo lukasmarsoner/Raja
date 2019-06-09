3f839e4d677d45d739973bc5815239916122022a
Lukas Marsoner <Lukas.marsoner@gmail.com>
2019-06-09 21:31:56 +0200

    Test corrections in Python file

--- consistency_check.py
+++ consistency_check.py
@@ -3,12 +3,12 @@ import anybadge
 
 with open('geschichte.json', 'r', encoding='utf-8') as jsonFile:
     jsonStr = jsonFile.read()
-    screens = json.loads(jsonStr)['screens']
+    _screens = json.loads(jsonStr)['screens']
 
 #Find indexes to screens
-indexes = []
-for i in range(len(screens)):
-    indexes.append(screens[i]['number'])
+screens = {}
+for i in range(len(_screens)):
+    screens[_screens[i]['number']] = _screens[i]
 
 #These arrays keep track of paths not taken
 openOptions = []
@@ -24,19 +24,15 @@ i = 0
 while not(atEnd):
     #See if we have been here before
     if i in openScreens:
-        #Find first entry for current screen
-        newOpt = openScreens.index(i)
-        #Move to next screen
-        i = openOptions[newOpt]
         screensSeenCounter[i] += 1
         #Remove existing entry
-        openScreens.pop(newOpt)
-        openOptions.pop(newOpt)
+        openScreens.pop(i)
+        openOptions.pop(i)
     else:
         if len(screens[i]['forwards']) > 1 and i not in branchesSeen:
             #Check that all lists have the required length
             if len(screens[i]['forwards']) != len(screens[i]['forwards']) or len(screens[i]['forwards']) != len(screens[i]['conditions']) or len(screens[i]['forwards']) != len(screens[i]['erlebnisse']):
-                   raise ValueError('Arrays at screen {} have inconsistend length'.format(indexes[i]))
+                   raise ValueError('Arrays at screen {} have inconsistend length'.format(i))
             
             #Make sure we dont get back to branching-points
             #Already seen and add old paths again
@@ -47,14 +43,14 @@ while not(atEnd):
         #Find the array-index the forward refers to
         try:
             if screens[i]['forwards'][0] != 999:
-                forward = indexes.index(screens[i]['forwards'][0])
+                forward = screens[i]['forwards'][0]
             else:
                 forward = 999
         except:
             print('Incorrect Forward')
         #Check loops - we should not see the same screen more than ten times
         if max(screensSeenCounter) > 10:
-            raise ValueError('Circular Forward found at screen {}'.format(indexes[i]))
+            raise ValueError('Circular Forward found at screen {}'.format(i))
         #Check if we have reached an endpoint
         if forward == 999:
             #End-Screens can be reached as many times as we want
@@ -65,9 +61,7 @@ while not(atEnd):
             totalNumerOfPaths += 1
             #If there any open screens left - move to one of them
             if len(openScreens) != 0:
-                newOpt = openScreens[0]
                 #Move to next screen
-                print(openOptions[0])
                 i = openOptions[0]
                 screensSeenCounter[i] += 1
                 #Remove existing entry
