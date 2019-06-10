b6093d38eef64475f19bb567c70001dba68ce257
Lukas Marsoner <Lukas.marsoner@gmail.com>
2019-06-10 12:02:04 +0200

    Update consistency script

--- consistency_check.py
+++ consistency_check.py
@@ -1,81 +1,44 @@
 import json
-import anybadge
+#import anybadge
+
+def moveToNextScreen(iScreen, options, screens):
+    _iScreen = iScreen
+    iScreen = screens[iScreen]['forwards'][options[iScreen]['current']]
+    options[_iScreen]['current'] += 1
+    if options[_iScreen]['current'] == options[_iScreen]['total']:
+        options[_iScreen]['current'] -= 1
+    return iScreen, options
 
 with open('geschichte.json', 'r', encoding='utf-8') as jsonFile:
     jsonStr = jsonFile.read()
     _screens = json.loads(jsonStr)['screens']
 
-#Find indexes to screens
 screens = {}
+options = {}
+nPaths = 0
 for i in range(len(_screens)):
     screens[_screens[i]['number']] = _screens[i]
+    options[_screens[i]['number']] = {'current': 0,
+                                      'total': len(screens[_screens[i]['number']]['forwards'])}
+    if options[_screens[i]['number']]['total'] > 1:
+        nPaths += options[_screens[i]['number']]['total']
 
-#These arrays keep track of paths not taken
-openOptions = []
-openScreens = []
-branchesSeen = []
-screensSeenCounter = [0 for i in screens]
-totalNumerOfPaths = 0
-atEnd = False
+del _screens
 
-#We will run through all possible paths - endings will
-#be marked by fowarting to 999
-i = 0
-while not(atEnd):
-    #See if we have been here before
-    if i in openScreens:
-        screensSeenCounter[i] += 1
-        #Remove existing entry
-        openScreens.pop(i)
-        openOptions.pop(i)
-    else:
-        if len(screens[i]['forwards']) > 1 and i not in branchesSeen:
-            #Check that all lists have the required length
-            if len(screens[i]['forwards']) != len(screens[i]['forwards']) or len(screens[i]['forwards']) != len(screens[i]['conditions']) or len(screens[i]['forwards']) != len(screens[i]['erlebnisse']):
-                   raise ValueError('Arrays at screen {} have inconsistend length'.format(i))
-            
-            #Make sure we dont get back to branching-points
-            #Already seen and add old paths again
-            branchesSeen.append(i)
-            for j in screens[i]['forwards'][1:]:
-                openOptions.append(j)
-                openScreens.append(i)
-        #Find the array-index the forward refers to
-        try:
-            if screens[i]['forwards'][0] != 999:
-                forward = screens[i]['forwards'][0]
-            else:
-                forward = 999
-        except:
-            print('Incorrect Forward')
-        #Check loops - we should not see the same screen more than ten times
-        if max(screensSeenCounter) > 10:
-            raise ValueError('Circular Forward found at screen {}'.format(i))
-        #Check if we have reached an endpoint
-        if forward == 999:
-            #End-Screens can be reached as many times as we want
-            #We set back the counter to falsly identify loops involving endpoints
-            screensSeenCounter[i] = 0
-            #Coming here means we found another full path
-            #Increase the corresponding counter
-            totalNumerOfPaths += 1
-            #If there any open screens left - move to one of them
-            if len(openScreens) != 0:
-                #Move to next screen
-                i = openOptions[0]
-                screensSeenCounter[i] += 1
-                #Remove existing entry
-                openScreens.pop(0)
-                openOptions.pop(0)
-            #Otherwise we are done
-            else:
-                atEnd = True
-        else:
-            i = forward
-            screensSeenCounter[i] += 1
+maxStepsToEnding = 200
+lengthPaths = []
 
-
-#From here on we create the badge to display in Gitlab
+for _iPath in range(nPaths):
+    iScreen = 0
+    lengthPaths.append(0)
+    for i in range(maxStepsToEnding):
+        iScreen, options = moveToNextScreen(iScreen, options, screens)
+        lengthPaths[-1] += 1
+        if iScreen == 888:
+            break
+        if i == maxStepsToEnding -1:
+            raise ValueError('Potential loop found!')
+    
 
 #Define thresholds: <2=red, <4=orange <8=yellow <10=green
 thresholds = {2: 'red',
@@ -83,6 +46,6 @@ thresholds = {2: 'red',
               6: 'yellow',
               10: 'green'}
 
-badge = anybadge.Badge('Anzahl Geschichten', str(int(totalNumerOfPaths)), thresholds=thresholds)
+badge = anybadge.Badge('Anzahl Geschichten', str(int(nPaths)), thresholds=thresholds)
 
 badge.write_badge('paths.svg')
