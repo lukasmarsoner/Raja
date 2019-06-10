a76b06cee11686bbf10c6028a8dfae77024cc8f2
Lukas Marsoner <Lukas.marsoner@gmail.com>
2019-06-10 15:56:40 +0200

    Correct mistake in counting the total number of paths

--- consistency_check.py
+++ consistency_check.py
@@ -15,30 +15,28 @@ with open('geschichte.json', 'r', encoding='utf-8') as jsonFile:
 
 screens = {}
 options = {}
-nPaths = 0
+nPaths = 1
 for i in range(len(_screens)):
     screens[_screens[i]['number']] = _screens[i]
     options[_screens[i]['number']] = {'current': 0,
                                       'total': len(screens[_screens[i]['number']]['forwards'])}
     if options[_screens[i]['number']]['total'] > 1:
-        nPaths += options[_screens[i]['number']]['total']
+        nPaths += options[_screens[i]['number']]['total'] - 1
 
 del _screens
 
 maxStepsToEnding = 200
-lengthPaths = []
 
 for _iPath in range(nPaths):
     iScreen = 0
-    lengthPaths.append(0)
     for i in range(maxStepsToEnding):
         iScreen, options = moveToNextScreen(iScreen, options, screens)
-        lengthPaths[-1] += 1
         if iScreen == 888:
             break
         if i == maxStepsToEnding -1:
             raise ValueError('Potential loop found!')
-    
+   
+print('All checks complete and successful!')
 
 #Define thresholds: <2=red, <4=orange <8=yellow <10=green
 thresholds = {2: 'red',
